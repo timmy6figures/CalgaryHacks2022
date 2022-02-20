81d9550fe43d1c54180a5f36d592f37bb9ab785a
Aryan Pandhare <53331522+aryan2000sp@users.noreply.github.com>
2022-02-19 21:02:42 -0600

    Added the header

--- Frontend/src/Components/Header/Header.jsx
+++ Frontend/src/Components/Header/Header.jsx
@@ -1,5 +1,49 @@
+import { ReactComponent as Mango } from "../../icons/mango_icon.svg";
+import { ReactComponent as Hamburger } from "../../icons/hamburger-menu.svg";
+import { ReactComponent as Close } from "../../icons/close.svg";
+import MenuList from "./MenuList";
+import { useState } from "react";
+
 const Header = () => {
-  return <div className="header">This is header</div>;
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  return (
+    <header className="header">
+      <nav className="header-navbar">
+        {/* Title wrapper */}
+        <div className="header-title-wrapper">
+          <section className="header-title">
+            <div className="mango-icon">
+              <Mango />
+            </div>
+
+            <h1>Mango Health</h1>
+          </section>
+
+          <section className="header-hamburger">
+            {isMenuOpen ? (
+              <Close
+                className="close-button"
+                onClick={() => {
+                  setIsMenuOpen(!isMenuOpen);
+                }}
+              />
+            ) : (
+              <Hamburger
+                className="hamburger-button"
+                onClick={() => {
+                  setIsMenuOpen(!isMenuOpen);
+                }}
+              />
+            )}
+          </section>
+        </div>
+
+        {/* Expanded menu items */}
+        <MenuList isOpen={isMenuOpen} />
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
