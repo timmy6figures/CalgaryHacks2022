99b4b3f2b474ad03bdb6d73fe88ef782d67e75ed
Kiernan McGuigan <46582079+kmcguigan1@users.noreply.github.com>
2022-02-21 15:26:19 -0700

    model interface

--- Frontend/src/Components/Main/Main.jsx
+++ Frontend/src/Components/Main/Main.jsx
@@ -1,51 +1,85 @@
 import { useState } from "react";
 import { useRef } from "react";
 
-const axios = require('axios').default
+import styles from "./main-style.module.scss";
+
+const axios = require("axios").default;
 
 const Main = () => {
-  // selectedFile contains information on the currently picked file.
-  // isFilePicked determines if a file has been picked or not.
-  const [selectedFile, setSelectedFile] = useState();
-  const [isFilePicked, setIsFilePicked] = useState(false);
-  const inputFile = useRef(null);
+	// selectedFile contains information on the currently picked file.
+	// isFilePicked determines if a file has been picked or not.
+	const [selectedFile, setSelectedFile] = useState();
+	const [isFilePicked, setIsFilePicked] = useState(false);
+	// const [hasInfo, setHasInfo] = useState(false);
+	const [infoData, setInfoData] = useState({ Enter: "Food" });
+	const inputFile = useRef(null);
 
-  // Handle get the file name
-  const changeHandler = (event) => {
-    setSelectedFile(event.target.files[0]);
-    setIsFilePicked(true);
-  };
+	// Handle get the file name
+	const changeHandler = (event) => {
+		setSelectedFile(event.target.files[0]);
+		setIsFilePicked(true);
+	};
 
-  const handleSubmission = () => {
-    if(!isFilePicked) return;
-    console.log(selectedFile);
-    axios.post("http://localhost:5000/barcodeImage", selectedFile, {
-        headers: {
-          'Content-Type':selectedFile.type,
-          'Access-Control-Allow-Origin': '*'//'http://localhost:5000/barcodeImage'
-        }
-    });
-    setSelectedFile(null);
-  };
+	const handleSubmission = () => {
+		if (!isFilePicked) return;
+		console.log(selectedFile);
+		axios.post("http://localhost:5000/barcodeImage", selectedFile, {
+			headers: {
+				"Content-Type": selectedFile.type,
+				"Access-Control-Allow-Origin": "*", //'http://localhost:5000/barcodeImage'
+			},
+		});
+		setSelectedFile(null);
+	};
 
-  const onButtonClick = () => {
-    inputFile.current.click();
-  };
+	const onButtonClick = () => {
+		inputFile.current.click();
+	};
 
-  return (
-    <div>
-      <input
-        type="file"
-        onChange={changeHandler}
-        ref={inputFile}
-        style={{ display: "none" }}
-      />
-      <button onClick={onButtonClick}>Select file</button>
-      <div>
-        <button onClick={handleSubmission}>Submit</button>
-      </div>
-    </div>
-  );
+	console.log(Object.keys(infoData))
+	console.log(typeof(Object.keys(infoData)))
+
+	return (
+		<div className={styles.container}>
+			<div className={styles.buttonContainer}>
+				<input type="file" onChange={changeHandler} ref={inputFile} style={{ display: "none" }} />
+				<button className={styles.button} onClick={onButtonClick}>Upload Image</button>
+				<button className={styles.button} onClick={handleSubmission}>Submit</button>
+			</div>
+			<Table className={styles.info} infoData={infoData} />
+		</div>
+	);
+};
+
+const Table = (infoData) => {
+	return (
+		<>
+			<table>
+				<thead>
+					<th>Nutrition</th>
+					<th>Info</th>
+				</thead>
+				<tbody>
+					<tr>
+						<td>Sugar</td>
+						<td>3g</td>
+					</tr>
+					<tr>
+						<td>Protein</td>
+						<td>13g</td>
+					</tr>
+					<tr>
+						<td>Carbs</td>
+						<td>26g</td>
+					</tr>
+					<tr>
+						<td>Fat</td>
+						<td>5g</td>
+					</tr>
+				</tbody>
+			</table>
+		</>
+	);
 };
 
 export default Main;
--- flask_server/flaskr/functionality/test_barcodes.py
+++ flask_server/flaskr/functionality/test_barcodes.py
@@ -1,14 +1,67 @@
 import os
 import cv2
-from barcodes import get_barcode_data
+from pyzbar import pyzbar
+
+def draw_barcode_to_image(object, image):
+    """
+    :param object: This is the barcode object, we use the position to draw on the image.
+    :param image: This is the image the barcode was extracted from.
+    :return: The image with the drawn on box surrounding the extracted barcode. 
+    """
+    image = cv2.rectangle(
+        image,
+        (object.rect.left, object.rect.top),
+        (object.rect.left+object.rect.width, object.rect.top+object.rect.height),
+        color=(0,255,0),
+        thickness=5
+    )
+    return image
+
+def extract_barcode_(img):
+    """
+    This function uses pyzbar to pull the barcode information from images fed into it.
+    :param img: Image to extract the barcode data from.
+    :return: The objects extracted from the image, this is a list of barcodes.
+    """
+    decoded_objects = pyzbar.decode(img)
+    return decoded_objects
+
+def get_barcode_data(img):
+    """This function takes in an image and transforms it into an extracted barcode.
+
+    Args:
+        img (np.array): This is an array of image data that represents the image sent to the server.
+
+    Raises:
+        InvalidBarcode: This error is thrown when no barcode can be extracted from an image.
+        MultipleBarcode: This error is thrown when multiple barcodes are found in the image.
+
+    Returns:
+        string: The barcode as a string extracted from the image.
+    """
+    barcode = extract_barcode_(img)
+    if(len(barcode) == 0):
+        print('no barcode')
+        return ""
+    if(len(barcode) > 1):
+        print('too many barcodes')
+        return ""
+    return barcode[0]
 
 def main():
     test_dir = "./test_imgs"
     for img_path in os.listdir(test_dir):
-        im = cv2.imread(os.path.join(test_dir, img_path))
-        cv2.imshow(img_path, cv2.resize(im, (600,600)))
-        barcode = get_barcode_data(im)
+        if('barcode3' not in img_path):
+            continue
+        im = cv2.resize(cv2.imread(os.path.join(test_dir, img_path)), (600,600))
+        cv2.imshow(img_path, im)
+        cv2.waitKey(0)
+        barcode_object = get_barcode_data(im)
+        barcode = barcode_object.data
+        print(barcode_object)
         print(barcode)
+        image = draw_barcode_to_image(barcode_object, im)
+        cv2.imshow(img_path, image)
         cv2.waitKey(0)
 
 if __name__ == '__main__':
